1d5f3b11366061c80f09f4882c5d970742651818
Vanessa Seawright <vanessaaseawright@gmail.com>
2020-06-17 23:19:36 -0300

    Atualizacao make test

--- src/test/java/steps/HomeSteps.java
+++ src/test/java/steps/HomeSteps.java
@@ -1,10 +1,12 @@
 package steps;
 
+import net.thucydides.core.annotations.Step;
 import pages.HomePage;
 
 public class HomeSteps {
     HomePage homepage;
 
+    @Step
     public void irParaPaginaDeLogin() {
         homepage.open();
         homepage.clicarEmLogin();
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,16 +1,19 @@
 package steps;
 
+import net.thucydides.core.annotations.Step;
 import pages.LoginPage;
 
 public class LoginSteps {
     LoginPage loginPage;
 
+    @Step
     public void InserirDadosDeLogin() {
         loginPage.preencherUserName("tomsmith");
         loginPage.preencherPassword("SuperSecretPassword!");
 
     }
 
+    @Step
     public void ClicaEmLogin() {
         loginPage.clicaEmLogin();
 
